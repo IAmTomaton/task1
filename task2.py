3fb76acca50e4a99ff303ef7730ecda0f814be95
Jaggernaut09 <mrhomka.com@gmail.com>
2020-03-11 19:24:44 +0500

    complete

--- task2.py
+++ task2.py
@@ -4,21 +4,33 @@ from queue import Queue
 
 class Graph:
 
-    def __init__(self, matrix):
-        self._matrix = matrix
-        self._count = len(self._matrix)
-        self._visited_points = [False for i in range(self._count)]
+    def __init__(self, adjacency_list):
+        self._adjacency_list = adjacency_list
+        self._count = len(self._adjacency_list)
+        self._visited_peaks = [False for i in range(self._count)]
 
-    def wide_walk(self):
+    def wide_walk(self, start_peak=0):
         queue = Queue()
-        queue.put(0)
+        queue.put(start_peak)
         while not queue.empty():
-            current_point = queue.get()
-            print(current_point)
-            self._visited_points[current_point] = True
-            for point in range(self._count):
-                if self._matrix[current_point][point] == 1 and not self._visited_points[point]:
-                    queue.put(point)
+            current_peak = queue.get()
+            yield current_peak
+            self._visited_peaks[current_peak] = True
+            for peak in self._adjacency_list[current_peak]:
+                if not self._visited_peaks[peak]:
+                    queue.put(peak)
+
+    def first_not_visited_peak(self):
+        for i in range(self._count):
+            if not self._visited_peaks[i]:
+                return i
+        return -1
+
+    def connectivity_components(self):
+        while self.first_not_visited_peak() != -1:
+            peaks = list(self.wide_walk(self.first_not_visited_peak()))
+            peaks.sort()
+            yield peaks
 
 
 def generate_argparser():
@@ -28,10 +40,10 @@ def generate_argparser():
     return parser
 
 
-def matrix_from_file(file):
+def adjacency_list_from_file(file):
     count = int(file.readline())
-    matrix = [[int(j) for j in file.readline().split(' ')] for _ in range(count)]
-    return matrix
+    adjacency_list = [[int(j) - 1 for j in file.readline().split(' ') if int(j) != 0] for _ in range(count)]
+    return adjacency_list
 
 
 def main():
@@ -39,12 +51,19 @@ def main():
     args = parser.parse_args()
     try:
         with open(args.input) as input_file:
-            matrix = matrix_from_file(input_file)
+            adjacency_list = adjacency_list_from_file(input_file)
     except FileNotFoundError:
         print('File ' + args.input + 'not found.')
         return
-    graph = Graph(matrix)
-    graph.wide_walk()
+    graph = Graph(adjacency_list)
+    with open(args.output, 'w') as output_file:
+        components = list(graph.connectivity_components())
+        output_file.write(str(len(components)))
+        for component in components:
+            output_file.write('\n')
+            for peak in component:
+                output_file.write(str(peak + 1) + ' ')
+            output_file.write('0')
 
 
 if __name__ == '__main__':
